f9a3ec0483afbfe458640dc3547385648a5a39f2
JesusCotlame <jcotlame@iwa.com.mx>
2024-02-14 16:34:40 -0600

    test
    Author: JesusCotlame

--- src/main/java/pixee/Main.java
+++ src/main/java/pixee/Main.java
@@ -7,18 +7,14 @@ public class Main {
     private static int myIntegerValue;
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-
-
         myIntegerValue = Integer.valueOf("3").intValue();
+
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
     }
 
-    public void log(){
-        logger.info(String.format("Processing AutoDelegate annotations %d", myIntegerValue));
-
-    }
+    public void log(){logger.info(String.format("Processing AutoDelegate annotations %d", myIntegerValue));}
 
     private static final Logger logger = Logger.getLogger(Main.class.getName());
 }
