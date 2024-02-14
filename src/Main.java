cf1280800b83fee5e9b864f36486e2ba50e510cd
JesusCotlame <jcotlame@iwa.com.mx>
2024-02-14 15:15:36 -0600

    feat(java):  test sonar cloud
    Author: JesusCotlame
    Refs: cotlame/test-sonar-cloud-integration

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.logging.Logger;
+
+public class Main {
+
+    private static int myIntegerValue;
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+
+
+        myIntegerValue = Integer.valueOf("3").intValue();
+    }
+
+    public void log(){
+        logger.info(String.format("Processing AutoDelegate annotations %d", myIntegerValue));
+
+    }
+
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+}
