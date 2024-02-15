b7ce0a995d108009a1da0bf7eade9586e915aebd
Francisco J Perez Gallardo <78174671+fjpgtt@users.noreply.github.com>
2024-02-15 16:44:57 -0600

    Add text multiple times

--- src/main/java/pixee/CodeWithoutErrors.java
+++ src/main/java/pixee/CodeWithoutErrors.java
@@ -5,18 +5,22 @@ public class CodeWithoutErrors {
     private int variable = 10;
 
     public int getVariable() {
+        System.out.println("Hello world");
         return variable;
     }
 
     public void setVariable(int variable) {
+        System.out.println("Hello world");
         this.variable = variable;
     }
 
     public int sum(int a, int b) {
+        System.out.println("Hello world");
         return a + b;
     }
 
     private int one(){
+        System.out.println("Hello world");
         return 1;
     }
 }
