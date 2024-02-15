632dce95b736d5d2ae6145f455b8717feb49e1e8
JesusCotlame <jcotlame@iwa.com.mx>
2024-02-14 18:05:05 -0600

    test sonar cloud
    Author: JesusCotlame

---
+++ src/main/java/pixee/CodeWithoutErrors.java
@@ -0,0 +1,18 @@
+package pixee;
+
+public class CodeWithoutErrors {
+
+    private int variable = 10;
+
+    public int getVariable() {
+        return variable;
+    }
+
+    public void setVariable(int variable) {
+        this.variable = variable;
+    }
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+}
--- src/main/java/pixee/Main.java
+++ src/main/java/pixee/Main.java
@@ -1,11 +1,17 @@
 package pixee;
+
+import java.util.logging.Logger;
+
 public class Main {
 
     private static int myIntegerValue;
 
     public static void main(String[] args) {
+
+        myIntegerValue = Integer.valueOf("3").intValue();
     }
 
-    public void log(){
-    }
+    public void log(){logger.info(String.format("Processing annotations %d", myIntegerValue));}
+
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
 }
