0c718027e30861741f98622fc738fca8a26019b2
pixeebot <help@pixee.ai>
2024-02-16 16:45:10 +0000

    Define a constant for a literal string that is duplicated n times (Sonar)

--- src/main/java/pixee/MyClass.java
+++ src/main/java/pixee/MyClass.java
@@ -2,13 +2,15 @@ package pixee;
 
 public class MyClass {
 
+private static final String HELLO_WORLD = "Hello world";
+
     private int number = 5;
     private boolean alwaysTrue(){
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
+        System.out.println(HELLO_WORLD);
+        System.out.println(HELLO_WORLD);
+        System.out.println(HELLO_WORLD);
+        System.out.println(HELLO_WORLD);
+        System.out.println(HELLO_WORLD);
         return true;
     }
 }
