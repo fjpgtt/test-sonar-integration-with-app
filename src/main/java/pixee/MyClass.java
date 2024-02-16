19140214c49b4ef2ee5bc06ddd18a0831071fed5
Francisco J Perez Gallardo <78174671+fjpgtt@users.noreply.github.com>
2024-02-16 10:03:24 -0600

    Add same text multiple times

--- src/main/java/pixee/MyClass.java
+++ src/main/java/pixee/MyClass.java
@@ -4,6 +4,10 @@ public class MyClass {
 
     private int number = 5;
     private boolean alwaysTrue(){
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
         return true;
     }
 }
