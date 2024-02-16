01b2085fe9504004bf3d13bf9bc83105da115512
Jesus Cotlame <jcotlame@iwa.com.mx>
2024-02-16 14:01:58 -0600

    test sonar cloud
    Author: JesusCotlame

---
+++ src/main/java/pixee/CodeWithErrors.java
@@ -0,0 +1,65 @@
+package pixee;
+
+public class CodeWithErrors {
+
+    // Error: Unused variable
+    private int unusedVariable;
+
+    // Error: Variable should not be public
+    public int variable = 10;
+
+    // Method getVariable should have a return type int
+    // Error: It should be public int getVariable()
+    public void getVariable() {
+        System.out.println("Hello world");
+        return variable;
+    }
+
+    // Method setVariable should not have a return type
+    // Error: It should be public void setVariable(int variable)
+    public int setVariable(int variable) {
+        System.out.println("Hello world");
+        this.variable = variable;
+    }
+
+    // Method sum should not have a return type
+    // Error: It should be public int sum(int a, int b)
+    public void sum(int a, int b) {
+        System.out.println("Hello world");
+        return a + b;
+    }
+
+    // Method one should not be private
+    // Error: It should be public int one()
+    private int one(){
+        System.out.println("Hello world");
+        return 1;
+    }
+
+    // Error: Empty catch block
+    public void methodWithEmptyCatchBlock() {
+        try {
+            // Code that might throw an exception
+        } catch (Exception e) {
+            // Empty catch block
+        }
+    }
+
+    // Error: Null pointer dereference
+    public void methodWithNullPointerError() {
+        String s = null;
+        int length = s.length(); // Attempting to access length of null reference
+    }
+
+    // Error: Division by zero
+    public void methodWithDivisionByZero() {
+        int x = 10;
+        int y = 0;
+        int result = x / y; // Division by zero
+    }
+
+    // Error: Unused private method
+    private void unusedMethod() {
+        // This method is not used anywhere in the code
+    }
+}
