b5852ee3897a30d6f21155d3e06eef6783c28cbf
pixeebot <help@pixee.ai>
2024-02-21 08:16:36 +0000

    Define a constant for a literal string that is duplicated n times (Sonar)

--- src/main/java/pixee/CodeWithErrors.java
+++ src/main/java/pixee/CodeWithErrors.java
@@ -2,6 +2,8 @@ package pixee;
 
 public class CodeWithErrors {
 
+private static final String HELLO_WORLD = "Hello world";
+
     // Error: Unused variable
     private int unusedVariable;
 
@@ -11,28 +13,28 @@ public class CodeWithErrors {
     // Method getVariable should have a return type int
     // Error: It should be public int getVariable()
     public void getVariable() {
-        System.out.println("Hello world");
+        System.out.println(HELLO_WORLD);
         return variable;
     }
 
     // Method setVariable should not have a return type
     // Error: It should be public void setVariable(int variable)
     public int setVariable(int variable) {
-        System.out.println("Hello world");
+        System.out.println(HELLO_WORLD);
         this.variable = variable;
     }
 
     // Method sum should not have a return type
     // Error: It should be public int sum(int a, int b)
     public void sum(int a, int b) {
-        System.out.println("Hello world");
+        System.out.println(HELLO_WORLD);
         return a + b;
     }
 
     // Method one should not be private
     // Error: It should be public int one()
     private int one(){
-        System.out.println("Hello world");
+        System.out.println(HELLO_WORLD);
         return 1;
     }
 
