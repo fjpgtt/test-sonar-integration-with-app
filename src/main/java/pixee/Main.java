278c37b3bfa55d37d868b45897e526e6a39ba264
Jesus Cotlame <jcotlame@iwa.com.mx>
2024-03-12 03:33:36 -0600

    test sonar cloud
    Author: JesusCotlame

--- src/main/java/pixee/Main.java
+++ src/main/java/pixee/Main.java
@@ -15,4 +15,26 @@ public class Main {
     }
 
     private static final Logger logger = Logger.getLogger(Main.class.getName());
+
+    public static void unzipFromHttpResponse(HttpResponse<InputStream> response, Path destDir) throws IOException {
+        try (InputStream inputStream = response.body();
+             ZipInputStream zipInputStream = new ZipInputStream(new BufferedInputStream(inputStream))) {
+
+            byte[] buffer = new byte[1024];
+            ZipEntry zipEntry = zipInputStream.getNextEntry();
+            while (zipEntry != null) {
+                if (!zipEntry.isDirectory()) {
+                    Path outputPath = destDir.resolve(zipEntry.getName());
+                    Files.createDirectories(outputPath.getParent());
+                    try (OutputStream outputStream = Files.newOutputStream(outputPath)) {
+                        int len;
+                        while ((len = zipInputStream.read(buffer)) > 0) {
+                            outputStream.write(buffer, 0, len);
+                        }
+                    }
+                }
+                zipEntry = zipInputStream.getNextEntry();
+            }
+        }
+    }
 }
