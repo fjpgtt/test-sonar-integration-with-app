31926c24ee099e9b9648b64cdb4b2a7a0b6a59d1
Jesus Cotlame <jcotlame@iwa.com.mx>
2024-03-15 17:47:45 -0600

    feat(action): update logs
    Author: JesusCotlame

--- src/main/java/pixee/Main.java
+++ src/main/java/pixee/Main.java
@@ -18,6 +18,9 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("Hello world");
         System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
     }
 
     public void log(){
